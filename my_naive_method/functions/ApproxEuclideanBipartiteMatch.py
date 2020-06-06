15ac9b3ff69875fe548b5e97694e698a1028fede
GQIN-A440 <673759941@QQ.COM>
2020-06-06 16:58:51 +0800

    Add execute exact bipartite match for local nodes in cell

--- my_naive_method/functions/ApproxEuclideanBipartiteMatch.py
+++ my_naive_method/functions/ApproxEuclideanBipartiteMatch.py
@@ -12,20 +12,36 @@ class ApproxEuclideanBipartiteMatch:
     def match(self, islonlat=False, distance_category='euclidean'):
         node_set_cell_id = self.assign_cell_id()
 
-        # Match within cell locally (randomly)
+        # === Overall parameters
+        pos_1, pos_2 = self.node_set
+        dist_mat = get_dist_mat(pos_1, pos_2, islonlat=islonlat, how=distance_category)
+
+        # === Match within cell locally (randomly)
         local_match_index, global_match_index = self.find_within_cell_nodes(node_set_cell_id)
-        local_match_result = local_match_index
-        # Match among cells globally (optimally)
-        node_set_global = [n[global_match_index[:, i],] for i, n in enumerate(self.node_set)]
+
+        # Iterate each cell and match
+        local_match_result = []
+        for cell_group_id in np.unique(local_match_index[:, -1]):
+            cell_local_match_index = local_match_index[local_match_index[:, -1] == cell_group_id, :2]
+
+            node_set_local = [n[cell_local_match_index[:, i], :] for i, n in enumerate(self.node_set)]
+
+            _, _, ind_1, ind_2 \
+                = ExactEuclideanBipartiteMatch(node_set_local).match(distance_category='manhattan')
+            local_match_result += [
+                [cell_local_match_index[i, 0], cell_local_match_index[j, 1]]
+                for (i, j) in zip(ind_1, ind_2)]
+
+        local_match_result = np.array(local_match_result)
+
+        # === Match among cells globally (optimally)
+        node_set_global = [n[global_match_index[:, i], :] for i, n in enumerate(self.node_set)]
 
         _, _, ind_1, ind_2 \
             = ExactEuclideanBipartiteMatch(node_set_global).match(distance_category='manhattan')
         global_match_result = np.concatenate((global_match_index[ind_1, 0].reshape(-1, 1),
                                               global_match_index[ind_2, 1].reshape(-1, 1)), axis=1)
-        # Combine match result
-        pos_1, pos_2 = self.node_set
-        dist_mat = get_dist_mat(pos_1, pos_2, islonlat=islonlat, how=distance_category)
-
+        # === Combine match result
         match_result = np.concatenate((local_match_result, global_match_result), axis=0)
         match_result = match_result[match_result[:, 0].argsort(), :]
 
@@ -57,7 +73,7 @@ class ApproxEuclideanBipartiteMatch:
 
     def find_within_cell_nodes(self, node_set_df):
         node_set_counts = node_set_df.groupby(['x_id', 'y_id', 'p'])['index'].apply(list).unstack().reset_index()
-        node_set_counts.p1 = node_set_counts.p1.apply(lambda d: d if isinstance(d, list) else []) # fill NaN with []
+        node_set_counts.p1 = node_set_counts.p1.apply(lambda d: d if isinstance(d, list) else [])  # fill NaN with []
         node_set_counts.p2 = node_set_counts.p2.apply(lambda d: d if isinstance(d, list) else [])
 
         node_set_counts.p1.apply(np.random.shuffle)
@@ -71,8 +87,13 @@ class ApproxEuclideanBipartiteMatch:
         node_set_counts['p2_global'] = node_set_counts.apply(lambda x: x.p2[x.num_within_cell:], axis=1)
 
         # Get local and global matching index pairs
+        local_cell_group_id = node_set_counts.reset_index().apply(lambda x: [x['index']] * x.num_within_cell, axis=1)
+        # p1 index, p2 index, & their cell group id
         local_match_index = np.concatenate((np.hstack(node_set_counts.p1_local).reshape((-1, 1)),
-                                            np.hstack(node_set_counts.p2_local).reshape((-1, 1))), axis=1).astype(int)
+                                            np.hstack(node_set_counts.p2_local).reshape((-1, 1)),
+                                            np.hstack(local_cell_group_id).reshape((-1, 1))), axis=1).astype(int)
+        # p1 index & p2 index
         global_match_index = np.concatenate((np.hstack(node_set_counts.p1_global).reshape((-1, 1)),
                                              np.hstack(node_set_counts.p2_global).reshape((-1, 1))), axis=1).astype(int)
+
         return local_match_index, global_match_index
--- my_naive_method/test.py
+++ my_naive_method/test.py
@@ -1,21 +1,26 @@
-import numpy as np
 import time
 from my_naive_method.functions.ExactEuclideanBipartiteMatch import *
 from my_naive_method.functions.ApproxEuclideanBipartiteMatch import *
 
 # Generate nodes
 N = 5000
-cell_size = N / 1e4
+cell_size = 2
+print('Cell size=%0.2f' % cell_size)
 node_set = [np.random.uniform(0, 10, (N, 2)), np.random.uniform(0, 10, (N, 2))]
 
 # === 0. Accurate ===
 start = time.time()
 eebm = ExactEuclideanBipartiteMatch(node_set)
-_, avg_distance, ind_1, ind_2 = eebm.match(distance_category='manhattan')
-print('[%0.4fs] EXACT avg_dist = %0.4f' % (time.time() - start, avg_distance))
+_, avg_distance_1, ind_1, ind_2 = eebm.match(distance_category='manhattan')
+t1 = time.time() - start
+print('[%0.2fs] EXACT avg_dist = %0.4f' % (t1, avg_distance_1))
 
 # === 1. Centralized ===
 start = time.time()
 aebm = ApproxEuclideanBipartiteMatch(node_set, cell_size=cell_size)
-_, avg_distance, ind_1, ind_2 = aebm.match(distance_category='manhattan')
-print('[%0.4fs] APPROX avg_dist = %0.4f' % (time.time() - start, avg_distance))
+_, avg_distance_2, ind_1, ind_2 = aebm.match(distance_category='manhattan')
+t2 = time.time() - start
+t2_1 = (t2 - t1) / t1 * 100
+avg_distance_2_1 = (avg_distance_2 - avg_distance_1) / avg_distance_1 * 100
+print('[%0.2fs (%0.1f%%)] APPROX avg_dist = %0.4f (+%0.1f%%)'
+      % (t2, t2_1, avg_distance_2, avg_distance_2_1))
